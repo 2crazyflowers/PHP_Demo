7a4b9b3f97ad0a6b5dc02e80f5dfaf6390a6da0d
Sara Bracewell <bracewell.sara@gmail.com>
2018-09-12 15:52:31 -0500

    if else statements

---
+++ ifstatement.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+  
+    if (3 > 10){
+        
+        echo "3 is less than 10";
+    
+    } elseif (11 === 5) {
+    
+        echo "11 is greater than 5";
+            
+    } else {
+        echo "none of the other conditions are true";
+    }
+    
+?>
+
+</body>
+</html>
