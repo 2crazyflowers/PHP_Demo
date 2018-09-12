9bb709a6e5bb1e6dd6f56c593a1ffb5673a25bf3
Sara Bracewell <bracewell.sara@gmail.com>
2018-09-12 15:26:40 -0500

    first commit

---
+++ variable.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    
+    $name = "<h1>Sara Bracewell</h1>";
+    $age = "46";
+    
+    echo $name;
+    echo $age;
+    echo $name . " " . $age;
+    
+    $hello = "<h1>Hello</h1>";
+    
+    echo $hello . " " . $name;
+    
+?>    
+
+</body>
+</html>
