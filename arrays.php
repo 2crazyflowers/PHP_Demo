3f3cb498a6d8c073ef41049822caa6f43998f923
Sara Bracewell <bracewell.sara@gmail.com>
2018-09-12 15:50:19 -0500

    math and arrays

---
+++ arrays.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+$numberList  = array(28, 5, 6, "545", '<h1>Hello</h1>', 999);
+
+// echo $numberList[0];
+// print_r(); is a built in function of php
+print_r($numberList);
+    
+    
+?>
+</body>
+</html>
---
+++ math.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    echo 56 + 45;
+    echo "<br>";
+    echo 56 - 45;
+    echo "<br>";
+    echo 64 / 8;
+    echo "<br>";
+    echo 64 * 8;
+    echo "<br>";
+    echo 45 * 8 / 4 + 3;
+    echo "<br>";
+    echo "<br>";
+
+    echo "<br>";
+
+    
+?>
+
+</body>
+</html>
