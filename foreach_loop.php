527ba00a92e16b222c6bdb7e08fa6bf33e5422e0
Sara Bracewell <bracewell.sara@gmail.com>
2018-09-13 12:09:04 -0500

    foreach loop

--- foreach_loop.php
+++ foreach_loop.php
@@ -6,5 +6,24 @@
 </head>
 <body>
 
+<?php 
+    
+    //most powerful loop
+    //works only with arrays
+    //goes through each value in array until it is finished
+    //do not need to tell it when to stop - no condition
+    $numbers = [325,458,689,850,967,1002];
+    
+    foreach($numbers as $number){
+        
+        echo $number . "<br>";
+        
+    }
+    
+    //only finishes until it goes through entire array
+    
+    
+?>
+
 </body>
 </html>
