6b07388bd6767acda1ffd1c35b9957aff1a04d13
Sara Bracewell <bracewell.sara@gmail.com>
2018-09-13 11:16:53 -0500

    comparison operators

---
+++ comp_operators.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<h2>Comparison Operators</h2>
+    <pre>
+    equal ==
+    identical ===
+    compare > < <= >= <>
+    not equal !=
+    not identical !==
+
+
+    </pre>
+
+<?php
+    
+    if(4 != 3) {
+        echo "this conditionis true" . "<br>";
+    }
+
+    if(4 === 4 && 5 < 10 ){
+        echo "both are true";
+    }
+    
+    
+?>
+
+</body>
+</html>
