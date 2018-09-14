43461bdbd515b8c8701a50438a336b121b82d70e
Sara Bracewell <bracewell.sara@gmail.com>
2018-09-14 10:56:55 -0500

    form external grabs info from user and form process does the php on the info collected

---
+++ form_external.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="form_process.php" method="post">
+    
+    <input type="text" placeholder="Enter Username" name="username"><br>
+    <br>
+    <input type="passord" placeholder="Enter Password" name="password"><br>
+    <br>
+    <!--    name submit currently is the only thing that is posted to the php above -->
+    <input type="submit" name="submit">
+    
+</form>
+
+
+</body>
+</html>
--- form_process.php
+++ form_process.php
@@ -47,33 +47,6 @@ if (isset($_POST['submit'])){
         echo "Welcome " . $username . "<br>";
     }
     
-    
-    
 }    
     
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-
-<form action="form.php" method="post">
-    
-    <input type="text" placeholder="Enter Username" name="username"><br>
-    <br>
-    <input type="passord" placeholder="Enter Password" name="password"><br>
-    <br>
-<!--    name submit currently is the only thing that is posted to the php above -->
-    <input type="submit" name="submit">
-    
-    
-    
-</form>
-
-
-</body>
-</html>
